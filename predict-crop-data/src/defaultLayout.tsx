e92408dd422c3afaf3ecb72c89bbc7cea202640a
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-11-25 22:39:43 +0200

    Add active links with color difference

--- predict-crop-data/src/defaultLayout.tsx
+++ predict-crop-data/src/defaultLayout.tsx
@@ -1,23 +1,59 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 const DefaultLayout = () => {
   return (
     <div>
-      <nav className="bg-red-400 flex justify-between py-6 px-32 font-noto">
+      <nav className="bg-red-400 flex justify-between py-6 px-32 font-noto items-center">
         <div>
-          <Link to={"/"}>Grow Initiative</Link>
+          <NavLink to={"/"}>Grow Initiative</NavLink>
         </div>
 
-        <div className="flex justify-between gap-5 w-1/3">
+        <div className="flex justify-between gap-5 w-1/3 items-center">
           <div className="flex gap-10">
-            <Link to={"/about"}>About</Link>
-            <Link to={"/services"}>Services</Link>
-            <Link to={"/contacts"}>Contacts</Link>
+            <NavLink
+              to={"/about"}
+              style={({ isActive }) => ({
+                color: isActive ? "blue" : undefined,
+              })}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to={"/services"}
+              style={({ isActive }) => ({
+                color: isActive ? "blue" : undefined,
+              })}
+            >
+              Services
+            </NavLink>
+            <NavLink
+              to={"/contacts"}
+              style={({ isActive }) => ({
+                color: isActive ? "blue" : undefined,
+              })}
+            >
+              Contacts
+            </NavLink>
           </div>
 
-          <div className="flex gap-3">
-            <Link to={"/login"}>Login</Link>
-            <Link to={"/signup"}>Sign Up</Link>
+          <div className="flex gap-3 items-center">
+            <NavLink
+              to={"/login"}
+              style={({ isActive }) => ({
+                color: isActive ? "blue" : undefined,
+              })}
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to={"/signup"}
+              style={({ isActive }) => ({
+                color: isActive ? "blue" : undefined,
+              })}
+              className={`bg-blue-600 px-5 py-2 rounded-md`}
+            >
+              Sign Up
+            </NavLink>
           </div>
         </div>
       </nav>
