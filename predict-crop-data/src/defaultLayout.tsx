556e86b9157831b6798b400498d4a98132070d54
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-11-25 21:58:04 +0200

    Add initial pip package structure and metadata files

--- predict-crop-data/src/defaultLayout.tsx
+++ predict-crop-data/src/defaultLayout.tsx
@@ -3,19 +3,22 @@ import { Outlet, Link } from "react-router-dom";
 const DefaultLayout = () => {
   return (
     <div>
-      <nav className="bg-red-400 flex justify-between py-6 px-10  ">
+      <nav className="bg-red-400 flex justify-between py-6 px-32 font-noto">
         <div>
-          <Link to={"/"}>Johnson</Link>
-        </div>
-        <div className="flex gap-5">
-          <Link to={"/about"}>About</Link>
-          <Link to={"/services"}>Services</Link>
-          <Link to={"/contacts"}>Contacts</Link>
+          <Link to={"/"}>Grow Initiative</Link>
         </div>
 
-        <div className="flex gap-3">
-          <Link to={"/login"}>Login</Link>
-          <Link to={"/signup"}>Sign Up</Link>
+        <div className="flex justify-between gap-5 w-1/3">
+          <div className="flex gap-10">
+            <Link to={"/about"}>About</Link>
+            <Link to={"/services"}>Services</Link>
+            <Link to={"/contacts"}>Contacts</Link>
+          </div>
+
+          <div className="flex gap-3">
+            <Link to={"/login"}>Login</Link>
+            <Link to={"/signup"}>Sign Up</Link>
+          </div>
         </div>
       </nav>
 
