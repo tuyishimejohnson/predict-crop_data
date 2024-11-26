8cd8827e87ae7fda227e28712ca66def8286d48f
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-11-26 11:54:59 +0200

    Add a button to predict

--- predict-crop-data/src/defaultLayout.tsx
+++ predict-crop-data/src/defaultLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, NavLink } from "react-router-dom";
 
-
-const activeLinks = ({ isActive }: { isActive: boolean }) => isActive ? 'text-blue-500' : undefined; 
+const activeLinks = ({ isActive }: { isActive: boolean }) =>
+  isActive ? "text-blue-500" : undefined;
 
 const DefaultLayout = () => {
   return (
@@ -13,38 +13,24 @@ const DefaultLayout = () => {
 
         <div className="flex justify-between gap-5 w-1/3 items-center">
           <div className="flex gap-10">
-            <NavLink
-              to={"/about"}
-              className={activeLinks}
-            >
+            <NavLink to={"/about"} className={activeLinks}>
               About
             </NavLink>
-            <NavLink
-              to={"/services"}
-              className={activeLinks}
-            >
+            <NavLink to={"/services"} className={activeLinks}>
               Services
             </NavLink>
-            <NavLink
-              to={"/contacts"}
-              className={activeLinks}
-        
-            >
+            <NavLink to={"/contacts"} className={activeLinks}>
               Contacts
             </NavLink>
           </div>
 
           <div className="flex gap-3 items-center">
-            <NavLink
-              to={"/login"}
-              className={activeLinks}
-              
-            >
+            <NavLink to={"/login"} className={activeLinks}>
               Login
             </NavLink>
             <NavLink
               to={"/signup"}
-              className={`bg-blue-600 px-5 py-2 rounded-md`}
+              className={`bg-blue-500 px-5 py-2 rounded-md text-white`}
             >
               Sign Up
             </NavLink>
--- predict-crop-data/src/home.tsx
+++ predict-crop-data/src/home.tsx
@@ -2,15 +2,17 @@ import irrigation from "./assets/images/erwan-hesry-1q75BReKpms-unsplash.jpg";
 
 const Home = () => {
   return (
-    <div className="relative">
+    <div className="relative text-white font-noto">
       <div className="absolute z-10">
-        <p className=" text-white text-2xl mt-24 pl-10">
-          Lorem ipsum dolor sait amet consectetur adipisicing elit. Officia rerum
-          impedit id perspiciatis atque, nostrum fuga voluptatem possimus! In
-          cupiditate sunt aspernatur nisi molestias assumenda architecto cumque
-          rerum tempore maiores?
-        </p>
-        <button></button>
+        <div className="px-10">
+          <p className="text-2xl mt-24">
+            Lorem ipsum dolor sait amet consectetur adipisicing elit. Officia
+            rerum impedit id perspiciatis atque, nostrum fuga voluptatem
+            possimus! In cupiditate sunt aspernatur nisi molestias assumenda
+            architecto cumque rerum tempore maiores?
+          </p>
+          <button className="mt-10 border px-8 py-4 rounded-full">Predict Now</button>
+        </div>
       </div>
       <div className="">
         <img
